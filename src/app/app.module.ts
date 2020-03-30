ad52e14ebfc6fc4ed520c2394f509ac6e0527a87
Salem Owino <salemowino18@gmail.com>
2020-03-30 11:58:45 +0300

    indented code

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
